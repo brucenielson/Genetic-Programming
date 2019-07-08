c7d086ce94a753079934f986c2fd59baa32b2e45
brucenielson <brucenielson@users.noreply.github.com>
2019-07-08 00:38:30 -0600

    Trying to create HTML annotation

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
+import Cython.Compiler.Options
+Cython.Compiler.Options.annotate = True
+
 setup(
     ext_modules = cythonize("gpcython.pyx", annotate=True)
 )
