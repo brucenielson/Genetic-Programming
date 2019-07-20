0326609482814771c7b4fbd21f481581665d039f
brucenielson <brucenielson@users.noreply.github.com>
2019-07-20 15:08:00 -0600

    About to start on faster version

--- geneticprogramming.py
+++ geneticprogramming.py
@@ -6,45 +6,18 @@ PARAM_NODE = 2
 CONST_NODE = 3
 
 # Node format
-# [TYPE #, Function #, value or param indx, [children], lock, node id]
+# [TYPE #, Function #, value or param indx, child1, child2, child3, lock, node id]
 # https://docs.scipy.org/doc/numpy/user/basics.rec.html
 # https://docs.scipy.org/doc/numpy/reference/generated/numpy.dtype.html#numpy.dtype
 
 
-class population:
-    def __init__(self, size):
-        pass
-
-class tree:
-    treecounter = 0
-    nodecounter = 0
-    def __init__(self):
-        self.id = tree.treecounter
-        tree.treecounter += 1
-        pass
 
 
+def runexperiment():
+    pass
 
-class node:
-    treecounter = 0
-    def __init__(self, fw, children):
-        self.function = fw.function
-        self.name = fw.name
-        self.children = children
-        self.id = node.treecounter
-        self.lock = False
-        node.treecounter += 1
+def main():
+    runexperiment()
 
-
-    def evaluate(self, inp):
-        results = [n.evaluate(inp) for n in self.children]
-        return self.function(results)
-
-    def display(self, indent=0):
-        if self.lock:
-            add = "*"
-        else:
-            add = ""
-        print( (' ' * indent) + self.name + add)
-        for c in self.children:
-            c.display(indent + 1)
+if __name__ == "__main__":
+    main()
